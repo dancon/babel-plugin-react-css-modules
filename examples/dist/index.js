4ac64e2250c584e905b827906b6ae1b0196e6b8b
sizhao <870301137@qq.com>
2019-01-23 22:27:38 +0800

    fix: just fot update npmjs doc

--- examples/dist/index.js
+++ examples/dist/index.js
@@ -1,6 +1,8 @@
-/**
- * 福卡明细
- */
+import classnames from "@pandolajs/classnames";
 import styles from './index.less';
 const ele = <div className={styles.name}></div>;
 const ele2 = <div className={styles["name"]}></div>;
+const ele3 = <div className={classnames(styles, {
+  "btn": true,
+  [`btn-${color}`]: true
+})}></div>;
--- examples/index.js
+++ examples/index.js
@@ -1,9 +1,7 @@
-/**
- * 福卡明细
- */
-
 import styles from './index.less'
 
 const ele = (<div className={styles.name}></div>)
 
 const ele2 = (<div className="name"></div>)
+
+const ele3 = (<div className={`btn btn-${color}`}></div>)
